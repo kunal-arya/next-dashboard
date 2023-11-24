6511d255eee3519c330c1fe211a5e576d2cba154
kunal-simplifii <kunal.arya@simplifii.com>
2023-11-24 08:54:05 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+    return (
+        <div>Customers Page</div>
+    )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+    return (
+        <div>Invoices Page</div>
+    )
+}
+
+export default Page
