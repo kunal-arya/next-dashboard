1707ead1194520ae64014932b62c2de6a2ff6bb4
kunal-arya <kunalaryad@gmail.com>
2023-11-24 09:02:31 +0530

    bug fix: fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
-import { lusitana } from "@/app/ui/font";
+import { lusitana } from "@/app/ui/fonts";
 import Image from 'next/image';
 
 export default function Page() {
